0107b4fbdac56b3fbc0a9365c617a4f50b17e536
Anna B Faizi <annanoor.b@gmail.com>
2017-05-27 11:59:44 -0700

    modified display table

--- timesheetLogic.js
+++ timesheetLogic.js
@@ -106,5 +106,5 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
   // Add each train's data into the table
   $("#train-table > tbody").append("<tr><td>" + tName + "</td><td>" + tDest + "</td><td>" +
-  tFreq + "</td><td>" + nextTrain + "</td><td>" + minAway + "</td></tr>");
+  tFreq + "</td><td>" + moment(nextTrain).format("HH:mm a") + "</td><td>" + minAway + "</td></tr>");
 });
